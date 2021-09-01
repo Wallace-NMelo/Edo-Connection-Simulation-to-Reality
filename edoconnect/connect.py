cf5b69e71d6c21c9a378fcf0cd9efe21758086fc
Wallace-NMelo <wallace.nm@outlook.com>
2021-09-01 19:35:20 -0300

    add README.md and connect.py

--- edoconnect/connect.py
+++ edoconnect/connect.py
@@ -1,27 +1,51 @@
 import sim
 
 
-def get_object_handle(Client_ID, handle_object):
-    # Get Object Handle
-    return sim.simxGetObjectHandle(Client_ID, handle_object, sim.simx_opmode_oneshot_wait)
+class EdoConnection:
+    def __init__(self, Client_ID, joint_numbers, max_Vel):
+        self.client_id = Client_ID
+        self.joint_numbers = joint_numbers
+        self.max_Vel = max_Vel
+        self.joint_handles = self._get_joint_handles()
 
+    def _get_joint_handles(self):
+        objects_handles = [sim.simxGetObjectHandle(self.client_id, f'joint_{i}',
+                                                   sim.simx_opmode_oneshot_wait) for i in
+                           range(1, self.joint_numbers + 1)]
 
-def get_motor_var(Client_ID):
-    # Get handles for robot parts, actuators and sensors:
-    res_R, motor_right = get_object_handle(Client_ID, 'Pioneer_p3dx_rightMotor')
-    res_L, motor_left = get_object_handle(Client_ID, 'Pioneer_p3dx_leftMotor')
-    return (res_L, res_R), (motor_left, motor_right)
+        return [object[1] for object in objects_handles if object[0] == 0]
 
+    def _set_upper_limit(self, joints):
+        for joint in joints:
+            sim.simxSetObjectFloatParameter(self.client_id, joint, sim.sim_jointfloatparam_upper_limit, self.max_Vel,
+                                            sim.simx_opmode_oneshot)
 
-def get_position(Client_ID):
+    def _set_target_position(self, joint, joint_mov):
+        if joint_mov is not None:
+            sim.simxSetJointTargetPosition(self.client_id, joint, joint_mov, sim.simx_opmode_oneshot)
 
-    # res, position = sim.simxGetObjectPosition(Client_ID, get_object_handle(Client_ID, 'Pioneer_p3dx'),
-    #                                           -1, sim.simx_opmode_oneshot_wait)
-    # if res > 0:
-    #     print('Error getting position')
-    #     return False
-    # return position
-    return None
+    def move_joints(self, j1_mov=None, j2_mov=None, j3_mov=None, j4_mov=None, j5_mov=None, j6_mov=None, j_mov_all=None):
+        for joint in self.joint_handles:
+            self._set_upper_limit(self.joint_handles)
+            if j_mov_all is None:
+                # Individual Movement for each Joint
+                joint1, joint2, joint3, joint4, joint5, joint6 = self.joint_handles
+                self._set_target_position(joint1, j1_mov)
+                self._set_target_position(joint2, j2_mov)
+                self._set_target_position(joint3, j3_mov)
+                self._set_target_position(joint4, j4_mov)
+                self._set_target_position(joint5, j5_mov)
+                self._set_target_position(joint6, j6_mov)
+            else:
+                # Same Movement for all Joints
+                for joint in self.joint_handles:
+                    self._set_target_position(joint, j_mov_all)
 
-def get_rotation(Client_ID):
-    pass
+    def mov_cartesian(self, x, y, z):
+        pass
+
+    def mov_to_default_pos(self):
+        pass
+
+    def mov_cancel(self):
+        pass
