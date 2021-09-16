8ed37ee724adc3cca3551d2d21029cdc6e3b16c1
Wallace-NMelo <wallace.nm@outlook.com>
2021-09-15 22:55:39 -0300

    update edorobot e edoconnection

--- edoconnect/main.py
+++ edoconnect/main.py
@@ -1,6 +1,7 @@
-import sim
-from connect import EdoController
 import time
+
+import sim, os
+from edoconnect import EdoController
 
 if __name__ == '__main__':
     print('Program Started')
@@ -9,11 +10,16 @@ if __name__ == '__main__':
         print('Connected to remote API server')
     else:
         print('Failed connecting to remote API server')
+    # sim.simxStopSimulation(clientID, sim.simx_opmode_oneshot_wait)
+    # sim.simxStartSimulation(clientID, sim.simx_opmode_oneshot_wait)
 
-    edo_robot = EdoController(Client_ID=clientID, joint_numbers=6, max_Vel=0.1)
-    edo_robot.move_joints(j2_mov=3.14/2)
+    edo_robot = EdoController(Client_ID=clientID)
+    edo_robot.move_joints(j2_mov=3.14 / 2)
+
     for i in range(60):
         print(f'{i}:{edo_robot.get_joints_relative_position()}')
-        #print(f'{i}: {edo_robot.get_object_position()}')
+        print(f'{i}: {edo_robot.get_object_position()}')
+        os.system('clear')
+        # print(f'{i}: {edo_robot.get_object_position()}')
         time.sleep(1)
     edo_robot.move_joints(j2_mov=-350)
